c5b67bdbc83d7ce30350c8d35b931367fdbd486c
KGBcomrade <vlasov.sviatoslav@gmail.com>
2018-10-20 02:31:44 +0300

    Added timer and second activity

---
+++ app/src/main/java/org/kgbcomrade/dz1/CountActivity.kt
@@ -0,0 +1,12 @@
+package org.kgbcomrade.dz1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_count)
+    }
+}
--- app/src/main/java/org/kgbcomrade/dz1/MainActivity.kt
+++ app/src/main/java/org/kgbcomrade/dz1/MainActivity.kt
@@ -1,12 +1,30 @@
 package org.kgbcomrade.dz1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ci = Intent(this, CountActivity::class.java)
+
+        object : CountDownTimer(2000, 1000) {
+
+            override fun onFinish() {
+                startActivity(ci)
+                finish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+        }.start()
     }
+
 }
