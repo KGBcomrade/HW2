06e780ae32dd0d7da9c01f1a7d18d87600897e05
KGBcomrade <vlasov.sviatoslav@gmail.com>
2018-10-20 02:51:23 +0300

    Added countdown to second activity

--- app/src/main/java/org/kgbcomrade/dz1/CountActivity.kt
+++ app/src/main/java/org/kgbcomrade/dz1/CountActivity.kt
@@ -2,11 +2,43 @@ package org.kgbcomrade.dz1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class CountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count)
+        val numbers = findViewById<TextView>(R.id.numbers)
+        val button = findViewById<Button>(R.id.button_timer)
+        var running = false
+        val cdt = object : CountDownTimer(1000000, 1000){
+            override fun onFinish() {
+                button.text = "Start"
+                running = false
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                numbers.text = ((1000000 - millisUntilFinished)/1000).toString()
+            }
+
+        }
+
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if(!running)
+                    cdt.start()
+                else
+                    cdt.cancel()
+                button.text = (if (running) "Start" else "Stop")
+                running = !running
+            }
+
+        })
+
+
     }
 }
